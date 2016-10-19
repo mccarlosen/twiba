c64306d72897c71c6c0178c938f5a72dcb391fc4
Carlos Meneses <osen.10112@gmail.com>
2016-10-19 16:41:09 -0400

    Added handle user

--- test1.js
+++ test1.js
@@ -3,7 +3,7 @@ var Twiba = require('twiba');
 settings = {}; // Custom config
 var tw = new Twiba(); // Without config - you can pass settings optional
 tw.get_user('osen_10112').then(usr => {
-    console.log("User ID: "+usr.id); // User ID
+    console.log("User ID: "+ usr.id +'\nHandle: '+ usr.screen_name); // User ID
 }).catch(err => {
     console.log(err);
 });
